ff52d736a4504097e44519512efe1a21958a7990
Ranit Saha <saharanit321@gmail.com>
2023-01-30 12:28:09 +0530

    First Commit

---
+++ Server/Interfaces/IUser.cs
@@ -0,0 +1,17 @@
+﻿using BlazorCRUDApp2.Shared.Models;
+
+namespace BlazorCRUDApp2.Server.Interfaces
+{
+    public interface IUser
+    {
+        public List<User> GetUsers();
+
+        public User GetUserById(int id);
+
+        public void AddUser(User user);
+
+        public void UpdateUser(User user);
+
+        public void DeleteUser(int id); 
+    }
+}
